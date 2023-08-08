16c143f0e320ca453d0213474f49fbb39377cc82
chenx <chenx@yunsilicon.com>
2023-08-08 21:37:11 +0800

    添加测试文件

---
+++ tests/test_config_vfs.py
@@ -0,0 +1,44 @@
+from lib.beryl import *
+
+
+
+def test_add_some_vfs():
+    pf0 = PF('p1p1')
+    vf = VF('p1p1_1',ip='1.1.1.1')
+    pf0.add_vf(vf)
+    vf = VF('p1p1_2',ip='1.1.1.2')
+    pf0.add_vf(vf)
+    vf = VF('p1p1_3',ip='1.1.1.3')
+    pf0.add_vf(vf)
+
+    pf1 = PF('p2p1')
+    vf = VF('p2p1_1',ip='1.1.2.1')
+    pf1.add_vf(vf)
+    vf = VF('p2p1_2',ip='1.1.2.2')
+    pf1.add_vf(vf)
+    vf = VF('p2p1_3',ip='1.1.2.3')
+    pf1.add_vf(vf)
+
+    server = BerylServer()
+    server.add_pf(pf0)
+    server.add_pf(pf1)
+
+    server.perform()
+
+
+def test_add_512vfs():
+    pf0 = PF('p1p1')
+    for i in range(512):
+        vf = VF(f'p1p1_{i}',ip=f'1.1.1.{i}')
+        pf0.add_vf(vf)
+
+    pf1 = PF('p2p1')
+    for i in range(512):
+        vf = VF(f'p2p1_{i}',ip=f'1.1.2.{i}')
+        pf1.add_vf(vf)
+
+    server = BerylServer()
+    server.add_pf(pf0)
+    server.add_pf(pf1)
+
+    server.perform()
