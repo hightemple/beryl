0e8cde22d3c3439b00908880251885181155b5e2
felix <13482394694@163.com>
2023-08-09 15:44:38 +0800

    支持端对端多条流。

--- lib/flow.py
+++ lib/flow.py
@@ -1,4 +1,5 @@
 import random
+import threading
 import time
 
 
@@ -8,12 +9,17 @@ import time
 # 3. flow使用iperf3进行测试
 # 4. flow可以根据pf或vf对象所包含的信息来给出iperf3的命令
 
-def create_flow(c_device, s_device):
-    flow = Flow(c_device, s_device)
-    return flow
+def create_flows(c_device, s_device, flow_num = 1):
+    flows = Flows()
+    for i in range(flow_num):
+        flows.add_flow(Flow(c_device, s_device))
+    return flows
+
+
+
 
 class Flow:
-    def __init__(self, c_device, s_device,packet_size=64, interval=1, type='udp', duration=10):
+    def __init__(self, c_device, s_device,packet_size=64, interval=1, type='udp', duration=2):
         self.packet_size = packet_size
 
         self.interval = interval
@@ -21,24 +27,47 @@ class Flow:
         self.duration = duration
         self.c_device = c_device
         self.s_device = s_device
+        self.log = None
 
     def start(self):
+
         # 生成一个随机的端口号
         port = random.randint(1024, 65535)
-        # 如果 self.c_device 是 PF 对象，则使用 PF 对象的信息来构造iperf3命令
 
-        c_iperf_cmd = f'iperf3 -c {self.s_device.ip} -u -B {self.c_device.ip} -l {self.packet_size} -i {self.interval} -t {self.duration} -p {port}'
-        s_iperf_cmd = f'iperf3 -s -u -B {self.s_device.ip} -l {self.packet_size} -i {self.interval} -t {self.duration} -p {port}'
+        # 生成在tmp下的文件，文件名为flow_时间戳, 时间戳转换为上海时间，且精确到毫秒
+        self.log = f'/tmp/flow_{time.strftime(f"%Y%m%d_%H%M%S_{port}", time.localtime())}.log'
+
+        c_iperf_cmd = f'nohup iperf3 -c {self.s_device.ip} -u -B {self.c_device.ip} -l {self.packet_size} -i {self.interval} -t {self.duration} -p {port} > {self.log} 2>&1 &'
+        s_iperf_cmd = f'nohup iperf3 -s -u -B {self.s_device.ip} -l {self.packet_size} -i {self.interval} -t {self.duration} -p {port} > {self.log} 2>&1 &'
 
         c_server = self.c_device.get_server()
         s_server = self.s_device.get_server()
 
 
         s_server.run_cmd(s_iperf_cmd)
-        time.sleep(1)
+        time.sleep(2)
         c_server.run_cmd(c_iperf_cmd)
+
+        time.sleep(self.duration + 1)
 
 
 
     def parse_result(self):
         pass
+
+class Flows:
+    def __init__(self):
+        self.flows = []
+
+    def add_flow(self, flow):
+        self.flows.append(flow)
+
+    def start(self):
+        threads = []
+        for flow in self.flows:
+            thread = threading.Thread(target=flow.start)
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
--- tests/test_flows.py
+++ tests/test_flows.py
@@ -1,5 +1,5 @@
 from lib.beryl import *
-from lib.flow import Flow
+from lib.flow import create_flows
 
 
 def test_flow_between_2pf():
@@ -21,8 +21,9 @@ def test_flow_between_2pf():
     print(s_pf0)
     print(c_pf0)
 
-    flow = Flow(c_pf0,s_pf0)
-    flow.start()
+    flows = create_flows(c_pf0, s_pf0)
+    flows.start()
+
 
 
 def test_flow_between_2vf():
@@ -46,8 +47,61 @@ def test_flow_between_2vf():
         serv.connect()
         serv.perform()
 
-    print(s_vf1)
+    print(s_vf0)
     print(c_vf0)
 
-    flow = Flow(c_vf0,s_vf0)
-    flow.start()
+    flows = create_flows(c_vf0, s_vf0)
+    flows.start()
+
+
+def test_flow_between_2_pf_4_flows():
+    s_pf0 = PF('p1p1',ip='6.6.6.1')
+
+
+    c_pf0 = PF('p1p1',ip='6.6.6.2')
+
+
+    server1 = BerylServer(ip='10.211.3.223')
+    server1.add_pf(s_pf0)
+    server2 = BerylServer(ip='10.211.3.224')
+    server2.add_pf(c_pf0)
+
+    for serv in [server1,server2]:
+        serv.connect()
+        serv.perform()
+
+    print(s_pf0)
+    print(c_pf0)
+
+    flows = create_flows(c_pf0, s_pf0, flow_num=4)
+    flows.start()
+
+
+
+
+def test_flow_between_2vf_4_flows():
+    s_pf0 = PF('p1p1')
+    s_vf0 = VF('p1p1_1',ip='6.6.6.1')
+    s_vf1 = VF('p1p1_2',ip='6.6.6.2')
+    s_pf0.add_vfs([s_vf0,s_vf1])
+
+    c_pf0 = PF('p1p1')
+    c_vf0 = VF('p1p1_1',ip='6.6.6.11')
+    c_vf1 = VF('p1p1_2',ip='6.6.6.12')
+    c_pf0.add_vfs([c_vf0,c_vf1])
+
+
+    server1 = BerylServer(ip='10.211.3.223')
+    server1.add_pf(s_pf0)
+    server2 = BerylServer(ip='10.211.3.224')
+    server2.add_pf(c_pf0)
+
+    for serv in [server1,server2]:
+        serv.connect()
+        serv.perform()
+
+    print(s_vf0)
+    print(c_vf0)
+
+    flows = create_flows(c_vf0, s_vf0, flow_num=4)
+    flows.start()
